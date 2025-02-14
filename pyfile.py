7ffb985e62309a5e7d50ae6b4b856239ce856c0b
Balaji <patilblaji2004@gmail.com>
2025-02-14 10:24:16 +0530

    initialcommit

---
+++ pyfile.py
@@ -0,0 +1,7 @@
+num1=int(input("enter the number:- "))
+num2=int(input("enter the number:- "))
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("after swapping number1: -",num1)
+print("after swapping number2: -",num2)
