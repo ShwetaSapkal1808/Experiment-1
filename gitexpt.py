e3bcba7a4d1ebdfd71a44b4701bbb9f23d3fa3f8
Srishti1877 <srishtismore77@gmail.com>
2025-03-21 10:22:39 +0530

    Update gitexpt.py

--- gitexpt.py
+++ gitexpt.py
@@ -5,3 +5,6 @@ print("Addition of numbers:",addition)
 print("Addition is")
 substraction=num1-num2
 print("Substraction of numbers:",substraction)
+
+multiply=num1*num2
+print("Multiplication is:",multiply)
